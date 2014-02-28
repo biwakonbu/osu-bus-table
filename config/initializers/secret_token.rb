37df6d7e7e5164162d8ab98db6556400d2d724d4
Ryo HIGASHIGAWA <ryo.higasigawa@gmail.com>
2014-02-28 16:43:17 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OsuApi::Application.config.secret_key_base = 'ef0b644aedb072c8318e33f3e2f50376e0c424c908572b74db324fe126202c07fd4976803c3b4099862a3ef87593551a3848c053c2ab7877bdab2e8e2b103582'
